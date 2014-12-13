26790c22a23bfaeff94f07ba40fb6f50d157cf7d
Keney Su <Home@Keneys-MacBook-Pro.local>
2014-12-13 00:46:01 -0800

    attempt to fix camera plugin

---
+++ js/author.js
@@ -0,0 +1,20 @@
+function DOMLoaded() {
+    document.addEventListener("deviceready", phonegapLoaded, false);
+}
+function phonegapLoaded() {
+
+}
+function takePicture() {
+    navigator.camera.getPicture(onSuccess,onFail,{ quality: 70, destinationType: Camera.DestinationType.FILE_URI, sourceType: Camera.PictureSourceType.PHOTOLIBRARY, encodingType: Camera.EncodingType.JPEG});
+}
+function takeCamera() {
+    navigator.camera.getPicture(onSuccess,onFail,{ quality: 70, destinationType: Camera.DestinationType.FILE_URI, sourceType: Camera.PictureSourceType.CAMERA, encodingType: Camera.EncodingType.JPEG});
+}
+function onSuccess(imageURI) {
+    var image = document.getElementById('image');
+    image.src = imageURI;
+    image.style.display = 'block';
+}
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
